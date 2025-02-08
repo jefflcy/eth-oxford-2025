35b8cff0b0e034b91bcd9488cf4c18eba1157a7d
lim-4158 <129579903+lim-4158@users.noreply.github.com>
2025-02-08 19:51:04 +0000

    added supabase db to store payment detail

---
+++ flaregate/src/lib/supabaseClient.js
@@ -0,0 +1,7 @@
+// filepath: /Users/kevinlim/Desktop/eth-oxford-2025/flaregate/src/lib/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
