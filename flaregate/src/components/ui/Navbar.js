3b314544f45a72adf0743e3fc94caff36fee2114
jepp <108892550+jefflcy@users.noreply.github.com>
2025-02-09 15:47:39 +0000

    feat: change homepage

--- flaregate/src/components/ui/Navbar.js
+++ flaregate/src/components/ui/Navbar.js
@@ -1,11 +1,8 @@
-// components/ui/NavBar.js
-"use client";
-
 export default function NavBar() {
   return (
     <nav className="w-full flex items-center justify-between px-6 py-4 fixed top-0 z-20 bg-white shadow">
       {/* Logo */}
-      <div className="text-2xl font-bold text-[#FF69B4] flaregate-font">
+      <div className="text-2xl font-bold text-[#FF69B4] flaregate-font cursor-pointer">
         FlareGate
       </div>
       {/* Right Section - Only contains the appkit button */}
